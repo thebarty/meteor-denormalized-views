2cba53f98d44decc834cfe4f97008cb27c39bad5
Henning <product_updates@grooveloft.com>
2016-07-30 23:11:45 +0200

    version bump for athmosphere

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'thebarty:denormalized-views',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Easily create "readonly" denormalized mongo-"views" (collections), p.e. for searchable tables',
   git: 'https://github.com/thebarty/meteor-denormalized-views',
   documentation: 'README.md'
